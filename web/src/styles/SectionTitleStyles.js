30d959e3ebebcf100f417bddb338eff54413b301
D1MK4-code <damianjakubas98@gmail.come>
2022-07-30 12:28:11 +0200

    Added single post styles

--- web/src/styles/SectionTitleStyles.js
+++ web/src/styles/SectionTitleStyles.js
@@ -2,3 +2,4 @@ import styled from 'styled-components';
 
 export const SectionTitleStyles = styled.h3``;
 export const TitleStyles = styled.h4``;
+export const MainTitleStyles = styled.h1``;
--- web/src/styles/posts/SinglePostStyles.js
+++ web/src/styles/posts/SinglePostStyles.js
@@ -1,10 +1,154 @@
 import styled from 'styled-components';
 
 export const SinglePostStyles = styled.div`
-  max-width: 700px;
+  max-width: 75rem;
   margin: 0 auto;
   .post-image {
-    height: 300px;
+    height: 22.5rem;
+    margin-bottom: 1.5rem;
+    width: calc(100% + 3rem);
+    margin-left: -1.5rem;
+  }
+  .post-header {
+    .published-at,
+    .categories-text,
+    .post-author {
+      display: flex;
+      align-items: center;
+      margin-bottom: 0.2rem;
+      font-size: 1.3rem;
+      text-align: left;
+      font-weight: 400;
+      font-family: 'DM Sans';
+      color: var(--secondary);
+      svg {
+        margin-right: 0.5rem;
+      }
+      a {
+        text-decoration: none;
+        &:hover,
+        &:focus {
+          color: var(--button);
+          text-decoration: underline;
+        }
+      }
+      a,
+      p,
+      span {
+        font-size: 1.3rem;
+        color: var(--secondary);
+      }
+    }
+    .post-author {
+      margin-bottom: 3rem;
+    }
+  }
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    font-family: 'DM Sans';
+    &:before {
+      display: none;
+    }
+  }
+  p,
+  a,
+  li,
+  span {
+    font-size: 1.5rem;
+    font-weight: 400;
+    color: var(--primary);
+    line-height: 1.6em;
+  }
+  strong {
+    font-weight: 700;
+    color: var(--primary);
+  }
+  em {
+    color: var(--primary);
+    font-weight: 500;
+  }
+  p {
     margin-bottom: 2rem;
+    text-align: justify;
+  }
+
+  h1 {
+    font-size: 2.3rem;
+  }
+  h2 {
+    font-size: 2.1rem;
+  }
+  h3 {
+    font-size: 2rem;
+  }
+  h4 {
+    font-size: 1.9rem;
+  }
+  h2,
+  h3,
+  h4 {
+    margin: 2rem 0 3rem;
+  }
+
+  .post-body {
+    .gatsby-image-wrapper {
+      margin: 3rem 0;
+    }
+    a {
+      color: var(--button);
+    }
+    ul,
+    ol {
+      padding: 0 0 1rem 1.5rem;
+      li {
+        margin-bottom: 0.8rem;
+      }
+    }
+    ul {
+      list-style: disc;
+    }
+    ol {
+      list-style: decimal;
+    }
+    .post-body-code {
+      margin: 2rem 0 !important;
+    }
+  }
+  #disqus_thread {
+    margin-top: 8rem;
+  }
+  @media (min-width: 575px) {
+    .post-image {
+      height: 30rem;
+      margin-bottom: 2rem;
+    }
+  }
+  @media (min-width: 768px) {
+    .post-image {
+      height: 35rem;
+      margin-bottom: 2.5rem;
+    }
+    h1 {
+      font-size: 2.7rem;
+    }
+    h2 {
+      font-size: 2.3rem;
+    }
+    h3 {
+      font-size: 2.2rem;
+    }
+    h4 {
+      font-size: 2.1rem;
+    }
+    p,
+    a,
+    li,
+    span {
+      font-size: 1.6rem;
+    }
   }
 `;
